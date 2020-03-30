d7e4c7dcdcc6e53b004dc643aa8706e7653b383a
Dawid Sielski <dawid.sielski@outlook.com>
2020-03-30 10:52:03 +0200

    Answers added

---
+++ answers/zadanie2.py
@@ -0,0 +1,18 @@
+# Napisz funkcję, która jako argument przyjmuje ciąg znaków. Jako output, zwróci liczbę liter występujących w ciągu więcej niż raz. Kod nie powinien być wrażliwy na wielkość liter. 
+# Przykład:
+# funkcja(“ABBA”) -> 2 (a i b występują po 2 razy)
+# funkcja(“aBcbA”) -> 2 (a i b się powtarza; wielkość liter jest ignorowana)
+# funkcja(“RabarbArka”) -> 3 (a,b i r)
+
+from collections import Counter
+
+
+test1 = 'ABBA'
+test2 = 'rabarbarka'
+
+
+def common_letters(seq):
+    return len([letter for letter, occurences in Counter(seq).items() if occurences > 1])
+
+
+print(common_letters(test2))
