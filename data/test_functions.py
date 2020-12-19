52f2532bd642a07b5dd5708fe3aec7b176d1d4da
seru71 <2869419+seru71@users.noreply.github.com>
2020-12-19 08:12:36 +0100

    added data from dev1-3

---
+++ data/test_functions.py
@@ -0,0 +1,17 @@
+def pretty_fast():
+    return sum([x for x in range(100_000)])
+
+
+def little_bit_slower():
+    nums = []
+    for i in range(1_000):
+        for j in range(1_000):
+            nums.append(i + j)
+
+
+def very_slow():
+    nums = []
+    for i in range(1_000):
+        for j in range(1_000):
+            for z in range(1_000):
+            	nums.append(i + j + z)
